04453edc8358ba4f32d5127302b09c156e518efb
samuelmbiya <samuelkmbiya@gmail.com>
2023-09-18 21:05:03 +0200

    recent updates

--- themessage.py
+++ themessage.py
@@ -13,7 +13,25 @@ query = soup.find_all('div', class_='cards')[0].contents
 
 N = len(query)
 
-for i in range(3, 5, 2):
+# find all sermons for alphabet letter
+for j in range(3, N, 2):
+    print('Name: '+query[j].text+ '\n' + 'Link: '+ query[j].get('href'))
+
+print("-"*100)
+
+# find and select a certain sermon
+
+sermon_no = 2
+
+paragraph_no = 3
+
+# has to be an odd number for some reason
+if sermon_no % 2 == 0:
+    sermon_no = sermon_no + 1
+
+sermon_no = sermon_no+2
+
+for i in range(sermon_no, sermon_no+2, 2):
     print('Name: '+query[i].text+ '\n' + 'Link: '+ query[i].get('href'))
     text_url = query[i].get('href')
 
@@ -25,8 +43,10 @@ for i in range(3, 5, 2):
     meta_data = text_query[0]
     title = meta_data.split("Preached")[0]
     preaching_text = text_query[1].split("\n") #split by paragraph number
-    K = len(preaching_text)
-    k = 6 #zero indexed list
-    # print(K)
-    print(preaching_text[k])
+    K = len(preaching_text) # no of paragraphs
+
+print(K)
+print(preaching_text[paragraph_no-1]) # zero indexed list
+
+# print(query)
 
